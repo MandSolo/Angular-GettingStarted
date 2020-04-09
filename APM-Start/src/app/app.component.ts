c293b98873b7f7d7ff4d8b0990539aeec32327fc
Mand Cashin <mandcashin@hotmail.co.uk>
2020-04-09 12:19:41 +0100

    feat: add  ProductListComponent

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
   template: `
     <div>
       <h1>{{ pageTitle }}</h1>
-      <div>My First Compoenet</div>
+      <div>My First Component</div>
     </div>
   `,
 })
