ef2cdc568745f33b699e612130a55eb62e969b45
Mand Cashin <mandcashin@hotmail.co.uk>
2020-04-10 13:52:56 +0100

    feat: add convert-to-spaces pipe

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListCompnent } from "./products/product-list.component";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
-  declarations: [AppComponent, ProductListCompnent],
+  declarations: [AppComponent, ProductListCompnent, ConvertToSpacesPipe],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
