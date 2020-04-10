fead5afc9327fe29ed344682f5ca2ff01469163c
Mand Cashin <mandcashin@hotmail.co.uk>
2020-04-10 13:33:15 +0100

    feat: add pipes to format product code and currency

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListCompnent } from "./products/product-list.component";
 
 @NgModule({
   declarations: [AppComponent, ProductListCompnent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
