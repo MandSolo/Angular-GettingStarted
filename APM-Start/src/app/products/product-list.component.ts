89abd4625017ecd5cdd456b9335ada3b9df045ae
Mand Cashin <mandcashin@hotmail.co.uk>
2020-04-10 18:23:38 +0100

    feat: add onclick functionality to stars

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -50,6 +50,10 @@ export class ProductListCompnent implements OnInit {
     this.listFilter = "cart";
   }
 
+  onRatingClicked(message: string): void {
+    this.pageTitle = "Product List: " + message;
+  }
+
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter(
